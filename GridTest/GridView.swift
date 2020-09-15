6e5c683e46ca6d41e344cb55fe6fe8db64d2c9d3
Gualtiero Frigerio <gualtiero.frigerio@gmail.com>
2020-09-15 17:20:50 +0200

    added columns and rows examples

--- GridTest/GridView.swift
+++ GridTest/GridView.swift
@@ -8,25 +8,55 @@
 import SwiftUI
 
 struct GridView: View {
-    var columns: [GridItem] = [
-        //GridItem(.flexible(minimum: 80, maximum: 100) , spacing: 10),
-        //GridItem(.fixed(50), spacing: 10),
-        GridItem(.adaptive(minimum: 90, maximum: 140), spacing: 10)
-        
+    var columns3Fixed: [GridItem] = [
+        GridItem(.fixed(100), spacing: 10),
+        GridItem(.fixed(100), spacing: 10),
+        GridItem(.fixed(100), spacing: 10)
+    ]
+    
+    var columns3Flexible: [GridItem] = [
+        GridItem(.flexible(minimum: 50, maximum: 150), spacing: 10),
+        GridItem(.fixed(140), spacing: 10),
+        GridItem(.fixed(140), spacing: 10)
+    ]
+    
+    var column3Adaptive: [GridItem] = [
+        GridItem(.adaptive(minimum: 90, maximum: 110), spacing: 10),
+        GridItem(.fixed(150), spacing: 10)
+    ]
+    
+    var column4Adaptive: [GridItem] = [
+        GridItem(.adaptive(minimum: 80, maximum: 150), spacing: 10)
+    ]
+    
+    var rows: [GridItem] = [
+        GridItem(.adaptive(minimum: 200, maximum: 250), spacing: 10),
+        GridItem(.fixed(100), spacing: 10)
     ]
     
     @ObservedObject var model:GridViewModel
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(
-                columns: columns,
-                alignment: .center,
-                spacing: 10,
-                pinnedViews: [.sectionHeaders, .sectionFooters]
+// uncomment to use colums layout
+//        ScrollView {
+//            LazyVGrid(
+//                columns: column4Adaptive,
+//                alignment: .center,
+//                spacing: 10,
+//                pinnedViews: []
+//            ) {
+//                ForEach(model.beers) { beer in
+//                    BeerView(beer: beer)
+//                }
+//            }
+//        }
+        
+// uncomment to use rows layout
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: rows, alignment: .center, spacing: 50, pinnedViews:[]
             ) {
                 ForEach(model.beers) { beer in
-                    BeerView(beer: beer)
+                    BeerView(beer: beer).border(Color.black)
                 }
             }
         }
